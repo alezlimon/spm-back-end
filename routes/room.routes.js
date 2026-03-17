98ebd8a9aabb8fb34bd4e86f5436bf583f03c4fd
Luana Aguilo <aguilo.luana@gmail.com>
2026-03-17 22:32:08 +0100

    add room routes update and config changes

--- routes/room.routes.js
+++ routes/room.routes.js
@@ -1,34 +1,77 @@
 const router = require("express").Router();
 const Room = require("../models/Room.model");
 
-// GET "/api/rooms" 
-router.get("/", (req, res, next) => {
-  Room.find()
-    .then((allRooms) => res.json(allRooms))
-    .catch((err) => res.json(err));
+
+// GET "/api/rooms" - Get all rooms
+router.get("/", async (req, res, next) => {
+  try {
+    const allRooms = await Room.find();
+    res.status(200).json(allRooms);
+  } catch (err) {
+    next(err);
+  }
 });
 
 
-// POST "/api/rooms" 
-router.post("/", (req, res, next) => {
-  const { roomNumber, type, pricePerNight } = req.body;
-  Room.create({ roomNumber, type, pricePerNight })
-    .then((newRoom) => res.json(newRoom))
-    .catch((err) => res.json(err));
+// GET "/api/rooms/:id" - Get one room
+router.get("/:id", async (req, res, next) => {
+  try {
+    const room = await Room.findById(req.params.id);
+
+    if (!room) {
+      return res.status(404).json({ message: "Room not found" });
+    }
+
+    res.status(200).json(room);
+  } catch (err) {
+    next(err);
+  }
 });
 
-// PUT "/api/rooms/:id" - Editar habitación
-router.put("/:id", (req, res, next) => {
-  Room.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    .then((updatedRoom) => res.json(updatedRoom))
-    .catch((err) => res.json(err));
+
+// POST "/api/rooms" - Create room
+router.post("/", async (req, res, next) => {
+  try {
+    const { roomNumber, type, pricePerNight } = req.body;
+
+    const newRoom = await Room.create({
+      roomNumber,
+      type,
+      pricePerNight
+    });
+
+    res.status(201).json(newRoom);
+  } catch (err) {
+    next(err);
+  }
 });
 
-// DELETE "/api/rooms/:id" - Eliminar habitación
-router.delete("/:id", (req, res, next) => {
-  Room.findByIdAndDelete(req.params.id)
-    .then(() => res.status(204).send())
-    .catch((err) => res.json(err));
+
+// PUT "/api/rooms/:id" - Update room
+router.put("/:id", async (req, res, next) => {
+  try {
+    const updatedRoom = await Room.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json(updatedRoom);
+  } catch (err) {
+    next(err);
+  }
 });
+
+
+// DELETE "/api/rooms/:id" - Delete room
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await Room.findByIdAndDelete(req.params.id);
+    res.status(204).send();
+  } catch (err) {
+    next(err);
+  }
+});
+
 
 module.exports = router;
